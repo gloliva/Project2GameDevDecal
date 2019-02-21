8fe3362960448f0806d29df975549379d246d162
Stephen Zhou <stephenzhou@berkeley.edu>
2019-02-20 22:05:56 -0800

    Fix health bar

---
+++ Library/Collab/Download/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D playerRigidbody;
+
+    float xAxis;
+    float yAxis;
+    public GameObject retryButton;
+
+    #region health_variables
+    public float maxHealth;
+    float currHealth;
+    public SimpleHealthBar healthBar;
+    #endregion
+
+    #region projectile_variables
+    public GameObject projectile;
+    public float fireSpeed;
+    public float reloadTime;
+    public float damage;
+    float reloadTimer = 0;
+    public bool specialAvailable;
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+
+        currHealth = maxHealth;
+
+        healthBar.UpdateBar(currHealth, maxHealth);
+
+        specialAvailable = false;
+
+        retryButton.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+        yAxis = Input.GetAxisRaw("Vertical");
+
+        moveFunction();
+        reloadTimer += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            ShootProjectile();
+        }
+
+        if (Input.GetKeyDown(KeyCode.K) && specialAvailable)
+        {
+            ShootSpecialAttack();
+        }
+    }
+
+    void moveFunction()
+    {
+        Vector2 movementVector = new Vector2(xAxis, yAxis);
+        movementVector = movementVector * 4;
+        playerRigidbody.velocity = movementVector;
+    }
+
+    void ShootProjectile()
+    {
+        if (reloadTimer > reloadTime)
+        {
+            reloadTimer = 0;
+            Vector2 FireDirection = new Vector2(1, 0);
+            FireDirection = FireDirection.normalized * fireSpeed;
+
+            // Create Missile
+            GameObject missile = (GameObject)Instantiate(projectile, transform.position, transform.rotation);
+            missile.GetComponent<Rigidbody2D>().velocity = FireDirection;
+            missile.GetComponent<MissileScript>().SetDamage(damage);
+
+            Destroy(missile, 5);
+        }
+    }
+
+    void ShootSpecialAttack()
+    {
+        if (reloadTimer > reloadTime)
+        {
+            reloadTimer = 0;
+            Vector2 FireDirection = new Vector2(1, 0);
+            FireDirection = FireDirection.normalized * fireSpeed;
+
+            // Create Missiles
+            GameObject missile1 = (GameObject)Instantiate(projectile, transform.position + (Vector3.up * 2.0f), transform.rotation);
+            missile1.GetComponent<Rigidbody2D>().velocity = FireDirection;
+            missile1.GetComponent<MissileScript>().SetDamage(damage);
+
+            GameObject missile2 = (GameObject)Instantiate(projectile, transform.position + (Vector3.up * 1.0f), transform.rotation);
+            missile2.GetComponent<Rigidbody2D>().velocity = FireDirection;
+            missile2.GetComponent<MissileScript>().SetDamage(damage);
+
+            GameObject missile3 = (GameObject)Instantiate(projectile, transform.position, transform.rotation);
+            missile3.GetComponent<Rigidbody2D>().velocity = FireDirection;
+            missile3.GetComponent<MissileScript>().SetDamage(damage);
+
+            GameObject missile4 = (GameObject)Instantiate(projectile, transform.position + (Vector3.down * 1.0f), transform.rotation);
+            missile4.GetComponent<Rigidbody2D>().velocity = FireDirection;
+            missile4.GetComponent<MissileScript>().SetDamage(damage);
+
+            GameObject missile5 = (GameObject)Instantiate(projectile, transform.position + (Vector3.down * 2.0f), transform.rotation);
+            missile5.GetComponent<Rigidbody2D>().velocity = FireDirection;
+            missile5.GetComponent<MissileScript>().SetDamage(damage);
+
+            Destroy(missile1, 5);
+            Destroy(missile2, 5);
+            Destroy(missile3, 5);
+            Destroy(missile4, 5);
+            Destroy(missile5, 5);
+
+            specialAvailable = false;
+        }
+    }
+
+    public void TakeDamage(float value)
+    {
+        currHealth -= value;
+        healthBar.UpdateBar(currHealth, maxHealth);
+
+        if (currHealth <= 0)
+        {
+            if (FindObjectOfType<Score>().m_Score > FindObjectOfType<Highscore>().m_Score)
+            {
+                FindObjectOfType<Highscore>().SetScore(FindObjectOfType<Score>().m_Score);
+                Data.highscore = FindObjectOfType<Highscore>().m_Score;
+            }
+            Die();
+            retryButton.SetActive(true);
+            
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(this.gameObject);
+    }
+}
+
---
+++ Library/Collab/Original/Assets/Scripts/MeleeEnemyScript.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemyScript : MonoBehaviour
+
+{
+
+    #region game_components
+    private Rigidbody2D rb;
+    private Animator anim;
+    #endregion
+
+    #region movement_variables
+    public float movespeed;
+    #endregion
+
+    #region targeting_variables
+    public Transform player;
+    public bool trackPlayer;
+    #endregion
+
+    #region attack_variables
+    private bool isAttacking;
+    private bool readyToAttack;
+    [SerializeField]
+    private float damage;
+    [SerializeField]
+    private float attackspeed;
+    float attackTimer;
+    [SerializeField]
+    private float hitboxTiming;
+    [SerializeField]
+    private float endAnimationTiming;
+    #endregion
+
+    #region
+    private float health;
+    #endregion
+
+    #region Unity_Functions
+    void Start()
+    {
+        //Spawn Enemy with a random y position, at x == 10, and a movespeed
+        trackPlayer = false;
+        isAttacking = false;
+        attackTimer = 2.0f;
+        damage = 1;
+        health = 2;
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        transform.position = new Vector2(10f, Random.Range(-5f, 5f));
+        //Set velocity vector of Enemy based off of start and end locations
+        Vector2 movementVector = new Vector2(transform.position.x, transform.position.y);
+        rb.velocity = movementVector.normalized * movespeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isAttacking)
+        {
+            return;
+        }
+
+        if (attackTimer <= 0 && trackPlayer)
+        {
+            Attack();
+        }
+        else if (trackPlayer)
+        {
+            MoveTowardsPlayer();
+        } else
+        {
+            MoveAcrossScreen();
+        }
+
+        attackTimer -= Time.deltaTime;
+    }
+    #endregion
+
+    #region movement_functions
+    private void MoveAcrossScreen()
+    {
+        // Move left across the screen
+        Vector2 direction = Vector2.left;
+
+        rb.velocity = direction.normalized * movespeed;
+
+        // Kill enemy when moved offscreen
+        if (rb.transform.position.x < -11f)
+        {
+            Debug.Log("Enemy moved offscreen");
+            Die();
+        }
+    }
+
+
+    private void MoveTowardsPlayer()
+    {
+        // Calculate movement vector. Player pos - Enemy pos = Direction of player relative to enemy
+        Vector2 direction = (player.position + Vector3.right - (Vector3.up * 0.5f)) - transform.position;
+
+        Debug.Log("direction: " + direction);
+        if (Mathf.Abs(direction.x) < 0.1f && Mathf.Abs(direction.y) < 0.1f)
+        {
+            rb.velocity = Vector2.zero;
+        } else
+        {
+            rb.velocity = direction.normalized * movespeed;
+        }
+    }
+    #endregion
+
+    #region attack_functions
+    public void Attack()
+    {
+        StartCoroutine(AttackRoutine());
+    }
+
+    IEnumerator AttackRoutine()
+    {
+        isAttacking = true;
+
+        // Trigger animation
+        anim.SetBool("Attacking", true);
+
+        yield return new WaitForSeconds(hitboxTiming);
+
+        Debug.Log("Cast hitbox now");
+        FindObjectOfType<AudioManager>().Play("Slash");
+
+        // Create hitbox
+        RaycastHit2D[] hits = Physics2D.BoxCastAll(rb.position + Vector2.left, Vector2.one, 0f, Vector2.zero, 0);
+        foreach (RaycastHit2D hit in hits)
+        {
+            Debug.Log(hit.transform.name);
+            if (hit.transform.CompareTag("Player"))
+            {
+                Debug.Log("Tons of damage");
+                hit.transform.GetComponent<PlayerController>().TakeDamage(damage);
+            }
+        }
+
+        // Wait for animation to end
+        yield return new WaitForSeconds(endAnimationTiming);
+
+        anim.SetBool("Attacking", false);
+        isAttacking = false;
+        attackTimer = 2.0f;
+    }
+    #endregion
+
+    #region health_functions
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            FindObjectOfType<Score>().AddScore(20);
+            Die();
+        } else
+        {
+            FindObjectOfType<AudioManager>().Play("Orc1");
+        }
+
+    }
+
+    private void Die()
+    {
+        FindObjectOfType<AudioManager>().Play("Explosion2");
+        Destroy(this.gameObject);
+    }
+    #endregion
+}
+
